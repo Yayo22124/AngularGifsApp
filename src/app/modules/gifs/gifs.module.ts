486f911355602c048943af22d609e6cc0d3d1840
Haziel Ortiz <ya-yo22124@outlook.com>
2024-04-02 17:14:32 -0600

    Initial Commit

---
+++ src/app/modules/gifs/gifs.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
+
+@NgModule({
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomePageComponent
+  ]
+})
+export class GifsModule { }
